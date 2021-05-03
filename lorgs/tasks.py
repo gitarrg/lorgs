7558c8f795c23eaced7f82ce2a7b7692d9cff15b
gitarrg <not telling :)>
2021-05-03 01:58:30 +0100

    fix celery config

--- lorgs/tasks.py
+++ lorgs/tasks.py
@@ -20,7 +20,7 @@ from lorgs.models.specs import WowSpec
 
 
 # make sure these are set
-os.environ["CELERY_BROKER_URL"] = os.getenv("CELERY_BROKER_URL") or os.getenv("REDIS_URL") or "redis://localhost:6379"
+os.environ["CELERY_BROKER_URL"] = os.getenv("CELERY_BROKER_URL") or os.getenv("REDISCLOUD_URL") or os.getenv("REDIS_URL") or "redis://localhost:6379"
 os.environ["CELERY_RESULT_BACKEND"] = os.getenv("CELERY_RESULT_BACKEND") or os.environ["CELERY_BROKER_URL"]
 
 celery = Celery("lorgs_celery")
