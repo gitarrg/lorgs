6fada6ca1645da7130b15fdfd3d1c5a1c4c54df9
gitarrg <not telling :)>
2021-09-10 21:40:24 +0100

    add Frost and Arcane Mage

--- lorgs/data/init_specs.py
+++ lorgs/data/init_specs.py
@@ -104,9 +104,6 @@ ROGUE_OUTLAW.supported = False
 DEATHKNIGHT_FROST.supported = False
 DRUID_FERAL.supported = False
 
-# rdps
-MAGE_ARCANE.supported = False
-MAGE_FROST.supported = False
 
 SUPPORTED_SPECS = [spec for spec in SPECS if spec.supported]
 
@@ -211,9 +208,18 @@ SHAMAN_RESTORATION.add_spell(  spell_id=198838, cooldown=60, duration=15, color=
 
 #################################################################################################################################################################################################
 #   Mage
+MAGE.add_spell(                spell_id=314793, cooldown=90,  duration=25, color=COL_VENTR, name="Mirrors of Torment",              icon="ability_revendreth_mage.jpg")
 MAGE.add_spell(                spell_id=314791, cooldown=60,  duration=3,  color=COL_NF,    name="Shifting Power",                  icon="ability_ardenweald_mage.jpg",               show=False)
+MAGE.add_spell(                spell_id=307443, cooldown=30,  duration=10, color=COL_KYR,   name="Radiant Spark",                   icon="ability_bastion_mage.jpg",                  show=False)
+MAGE.add_spell(                spell_id=324220, cooldown=180, duration=25, color=COL_NECRO, name="Deathborne",                      icon="ability_maldraxxus_mage.jpg")
+MAGE.add_spell(                spell_id=116011, cooldown=45,  duration=12,                  name="Rune of Power",                   icon="spell_mage_runeofpower.jpg",                show=False)
 MAGE_FIRE.add_spell(           spell_id=190319, cooldown=60,  duration=10, color="#e3b02d", name="Combustion",                      icon="spell_fire_sealoffire.jpg")
 MAGE_FIRE.add_spell(           spell_id=153561, cooldown=45,                                name="Meteor",                          icon="spell_mage_meteor.jpg",                     show=False)
+MAGE_ARCANE.add_spell(         spell_id=12042,  cooldown=100, duration=10,                  name="Arcane Power",                    icon="spell_nature_lightning.jpg")  # 2min base cd / reduced by conduit
+MAGE_ARCANE.add_spell(         spell_id=321507, cooldown=45,  duration=8,                   name="Touch of the Magi",               icon="spell_mage_icenova.jpg")
+MAGE_FROST.add_spell(          spell_id=12472,  cooldown=60,  duration=20,                  name="Icy Veins",                       icon="spell_frost_coldhearted.jpg") # 3min base cd / reduced by conduit
+MAGE_FROST.add_spell(          spell_id=257537, cooldown=45,                                name="Ebonbolt",                        icon="artifactability_frostmage_ebonbolt.jpg")
+MAGE_FROST.add_spell(          spell_id=84714,  cooldown=60,                                name="Frozen Orb",                      icon="spell_frost_frozenorb.jpg")
 
 #################################################################################################################################################################################################
 #   Warlock
