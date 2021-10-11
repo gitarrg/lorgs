f612cc6a6501d838cf1b14a819dcc95ab9e5aea9
gitarrg <not telling :)>
2021-10-11 18:24:29 +0100

    Navbar: add indicator for active role Navbar: sort specs by name Navbar: split clases into files

--- lorgs/frontend/components/Navbar/Navbar.jsx
+++ lorgs/frontend/components/Navbar/Navbar.jsx
@@ -1,41 +1,12 @@
 
-import React from 'react'
 import { useSelector } from 'react-redux'
 
-import NavbarBossButton from "./NavbarBossButton.jsx"
 import NavbarSpecGroup from "./NavbarSpecGroup.jsx"
 import { MODES } from '../../store/ui.js'
-import { get_bosses } from '../../store/bosses.js'
+import NavbarBossGroup from './NavbarBossGroup.jsx'
 
 
 ////////////////////////////////////////////////////////////////////////////////
-
-
-function NavbarGroup({children, className}) {
-
-    if (!children) return null
-    return (
-        <div className={`navbar_group p-1 bg-dark border rounded ${className}`}>
-            {children}
-        </div>
-    )
-}
-
-function NavbarBossGroup() {
-    const bosses = useSelector(state => get_bosses(state))
-
-    return (
-        <NavbarGroup className = "navbar_boss">
-            {Object.values(bosses).map(boss =>
-                <NavbarBossButton key={boss.full_name_slug} boss={boss} />
-            )}
-        </NavbarGroup>
-    )
-}
-
-
-////////////////////////////////////////////////////////////////////////////////
-
 
 export default function Navbar() {
 
@@ -44,13 +15,7 @@ export default function Navbar() {
     return (
         <div className="ml-auto">
             <div className="navbar_container">
-
-                { mode == MODES.SPEC_RANKING && (
-                    <NavbarGroup>
-                        <NavbarSpecGroup />
-                    </NavbarGroup>
-                )}
-
+                { mode == MODES.SPEC_RANKING && <NavbarSpecGroup /> }
                 <NavbarBossGroup />
             </div>
         </div>
