8be800647c79298f8d349ceb8d7095016e91ac85
gitarrg <not telling :)>
2021-10-12 02:40:52 +0100

    add ability to click group headers, to toogle all spells/child-buttons at once

--- lorgs/frontend/components/SettingsBar/DisplaySettings.jsx
+++ lorgs/frontend/components/SettingsBar/DisplaySettings.jsx
@@ -2,22 +2,44 @@
 
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import ButtonGroup from './shared/ButtonGroup.jsx'
+import ButtonGroup, { ButtonGroupContext } from './shared/ButtonGroup.jsx'
 import { update_settings } from '../../store/ui.js'
 
 
 function Button({attr_name, icon_name, tooltip=""}) {
 
+    ////////////////////////
+    // Hooks
+    //
     const attr_value = useSelector(state => state.ui.settings[attr_name])
     const dispatch = useDispatch()
     const disabled = attr_value ? "" : "disabled"
+    const [{group_active, group_source}, set_group_active] = React.useContext(ButtonGroupContext)
+
 
     function onClick() {
+        const new_value = !attr_value
+
         dispatch(update_settings({
-            [attr_name]: !attr_value
+            [attr_name]: new_value
         }))
+
+        if (new_value) {
+            set_group_active({group_active: new_value, group_source: "child"})
+        }
     }
 
+    // see SpellButton,jsx
+    React.useEffect(() => {
+        if (group_source !== "group") { return}
+        dispatch(update_settings({
+            [attr_name]: group_active
+        }))
+    }, [group_active])
+
+    ////////////////////////
+    // Render
+    //
     return (
         <div data-tooltip={tooltip}>
             <div
--- lorgs/frontend/components/SettingsBar/SpellSettings/SpellButton.jsx
+++ lorgs/frontend/components/SettingsBar/SpellSettings/SpellButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
 import { get_spell, set_spell_visible, get_spell_visible } from '../../../store/spells.js'
+import { ButtonGroupContext } from '../shared/ButtonGroup.jsx'
 
 
 /* to avoid react rerenders when clicking the <a> tags */
@@ -12,11 +13,11 @@ function no_link(e) {
 
 export default function SpellButton({spec, spell_id, onClick}) {
 
-
     // Hooks
     const dispatch = useDispatch()
     const spell = useSelector(state => get_spell(state, spell_id))
     const visible = useSelector(state => get_spell_visible(state, spell.spell_id))
+    const [{group_active, group_source}, set_group_active] = React.useContext(ButtonGroupContext)
 
     if (!spec) { return null}
 
@@ -26,15 +27,43 @@ export default function SpellButton({spec, spell_id, onClick}) {
 
     // onClick Callback
     function toggle_spell() {
+        const new_value = !visible
 
+        // Toggle the spell itself
         dispatch(set_spell_visible({
             spell_id: spell.spell_id,
-            visible: !visible
+            visible: new_value
         }))
-        onClick && onClick(!visible)
 
+        // if the spell became active make sure to also enable the parent group.
+        // passing "child" as group_source to differenciate between clicks on the
+        // group itself and triggers like these, which should only affect the
+        // group itself, but not its children
+        if (new_value) {
+            set_group_active({group_active: new_value, group_source: "child"})
+        }
+
+        // Invoke any additional onClick Callbacks
+        onClick && onClick(new_value)
     }
 
+    // Listen to State Changes of the parent Group
+    React.useEffect(() => {
+
+        // if the state was not changed from the group level,
+        // we ignore the event.
+        // (eg.: the state was change from another child in the group)
+        if (group_source !== "group") { return}
+
+        // otherwise (eg.: the entire group was toggled)
+        // we match the spells state to the parent state
+        dispatch(set_spell_visible({
+            spell_id: spell.spell_id,
+            visible: group_active
+        }))
+    }, [group_active])
+
+    ////////////////////////////////
     // Render
     return (
         <a onClick={no_link} href="" data-wowhead={spell.tooltip_info}>
