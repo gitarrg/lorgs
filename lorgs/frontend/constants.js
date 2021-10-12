a22cd4e1bee405c124a0413dbb5232b50a8eece9
gitarrg <not telling :)>
2021-10-12 01:11:28 +0100

    use svg logo in header

--- lorgs/frontend/constants.js
+++ lorgs/frontend/constants.js
@@ -3,11 +3,10 @@
 
 export const WCL_URL = "https://www.warcraftlogs.com"
 
-export const LOGO_URL = "/static/images/logo_bw.png";
+export const LOGO_URL = "/static/images/logo.svg";
 
 
 // Timeline Vars
 export const LINE_HEIGHT = 28
 
 export const DEFAULT_ZOOM = 4
-
