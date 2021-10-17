f2fc4b59ae7adb246df4590b44a7bbca88d149a5
gitarrg <not telling :)>
2021-10-17 16:57:31 +0100

    inline logo svgs

--- lorgs/frontend/components/HeaderLogo.tsx
+++ lorgs/frontend/components/HeaderLogo.tsx
@@ -1,13 +1,6 @@
 import { Link } from "react-router-dom"
+import LorrgsLogo from "../assets/LorrgsLogo"
 import styles from "./HeaderLogo.scss"
-
-
-const LOGO_SVG = (
-<svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 200 200">
-    <path d="M41 33l15-19 15 19H61v134h78v-10l20 15-20 15v-10H51V33H41zm30-20h33v116h55v28h-8v-20.031H96V21H71v-8z"
-    fillRule="evenodd"/>
-</svg>
-)
 
 
 export default function HeaderLogo({wow_class = "wow-boss"}) {
@@ -15,9 +8,7 @@ export default function HeaderLogo({wow_class = "wow-boss"}) {
     return (
         <div className={styles.container}>
             <Link to="/">
-                <svg className={`${styles.logo} ${wow_class} icon-l bg-dark rounded grow-when-touched`}>
-                    {LOGO_SVG}
-                </svg>
+                <LorrgsLogo className={`${styles.logo} ${wow_class} icon-l bg-dark rounded grow-when-touched`} />
             </Link>
         </div>
     )
