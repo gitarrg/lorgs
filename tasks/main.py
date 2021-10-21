071ab944098bb9aedbd559c789bee56cf1db5394
gitarrg <not telling :)>
2021-10-21 02:54:12 +0100

    cleanup

--- tasks/main.py
+++ tasks/main.py
@@ -1,9 +1,7 @@
-"""Entry point for Google Cloud Functions.
+"""Entry points for all our Cloud Functions.
 
     Note:
         this file has to be called "main.py"
-
-    Defines the entry points for all our Cloud Functions.
 
 """
 # IMPORT STANDARD LIBRARIES
@@ -23,20 +21,24 @@ def load_spec_rankings(request):
     """Load the Spec Ranking Data from Warcraftlogs and save it to the Database.
 
     Arguments should be passed as request args.
+    Args:
+        boss_slug (str)
+        spec_slug (str)
+        limit (int, optional): default=50
+        clear (bool, optional): default=False
 
     """
     # Get inputs
     boss_slug = request.args.get("boss_slug", type=str)
     spec_slug = request.args.get("spec_slug", type=str)
-    limit = request.args.get("limit", type=int, default=5)
+    limit = request.args.get("limit", type=int, default=50)
     clear = request.args.get("clear", default=False, type=json.loads)
-    print(f"I'm loading: {boss_slug} vs {spec_slug} | (limit={limit})")
+    print(f"loading: {boss_slug} vs {spec_slug} | (limit={limit})")
     if boss_slug is None or spec_slug is None:
         return f"missing boss or spec ({boss_slug} / {spec_slug})"
 
     # get spec ranking object
     ranking = warcraftlogs_ranking.SpecRanking.get_or_create(boss_slug=boss_slug, spec_slug=spec_slug)
-    print("ranking", ranking)
     if not ranking.boss:
         return "invalid boss"
     if not ranking.spec:
