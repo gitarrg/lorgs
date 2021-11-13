9ddb3506bf1df7fe32a73ae90efb86ed777d1e41
gitarrg <not telling :)>
2021-11-13 20:30:47 +0000

    user_reports: fix mixed spec players

--- frontend/src/components/ReportNavbar/PlayerWidget.tsx
+++ frontend/src/components/ReportNavbar/PlayerWidget.tsx
@@ -4,21 +4,22 @@ import styles from "./PlayerWidget.scss"
 import type Actor from '../../types/actor'
 import { get_spec } from '../../store/specs'
 import { useAppSelector } from '../../store/store_hooks'
+import { get_class } from '../../store/classes'
 
 
 export default function PlayerWidget({player} : {player: Actor}) {
 
     const field_name = `player[${player.source_id}]`
     const spec = useAppSelector(state => get_spec(state, player.spec))
-    if (!spec) { return null }
-
-    const className = `${styles.container} wow-${spec.class.name_slug}`
+    const wow_class_name = spec?.class.name_slug || player.class
+    const wow_class = useAppSelector(state => get_class(state, wow_class_name))
+    const className = `${styles.container} wow-${wow_class_name}`
 
     ////////////////////////////////
     // Render
     return (
         <SelectGridItem field_name={field_name} className={className}>
-            <Icon spec={spec} size="s" />
+            <Icon spec={spec || wow_class} size="s" />
             <span>{player.name}</span>
         </SelectGridItem>
     )
