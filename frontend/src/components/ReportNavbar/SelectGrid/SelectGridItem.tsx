cd6317679b4841a36ea84d31042eafd72b28560a
gitarrg <not telling :)>
2021-12-21 04:37:48 +0000

    restrict GridSelection for non-patron users

--- frontend/src/components/ReportNavbar/SelectGrid/SelectGridItem.tsx
+++ frontend/src/components/ReportNavbar/SelectGrid/SelectGridItem.tsx
@@ -2,6 +2,7 @@ import styles from "./SelectGrid.scss"
 import { useFormContext, useWatch } from 'react-hook-form'
 import { useContext, useEffect } from "react";
 import { SelectGroupContext } from "./SelectGroup";
+import useUser from "../../../routes/auth/useUser";
 
 
 type SelectGridItemProps = {
@@ -21,9 +22,19 @@ export default function SelectGridItem({field_name, className="", children}: Sel
     const { setValue } = useFormContext();
     const group_selected = useContext(SelectGroupContext)
 
+    const user = useUser()
+    const user_can_multiselect = user.permissions.includes("user_reports")
+    const field_parent = field_name.split("[")[0]
+
     ////////////////////////////////
     // Callbacks
     function onClick() {
+
+        // force deseletion for free users
+        if (!user_can_multiselect) {
+            setValue(field_parent, [])
+        }
+
         setValue(field_name, !selected)
     }
 
