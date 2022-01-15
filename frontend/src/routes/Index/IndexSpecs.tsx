ea5ee1955fc46ee82c2eba5b565fc45e39971346
gitarrg <not telling :)>
2022-01-15 03:07:58 +0000

    update IndexLinks to include Difficulty

--- frontend/src/routes/Index/IndexSpecs.tsx
+++ frontend/src/routes/Index/IndexSpecs.tsx
@@ -9,6 +9,7 @@ import styles from "./IndexSpecs.scss"
 
 
 const DEFAULT_BOSS = "the-tarragrue"
+const DEFAULT_DIFF = "mythic"
 
 
 function SpecButton({spec_slug=""}) {
@@ -17,7 +18,7 @@ function SpecButton({spec_slug=""}) {
     if (!spec) { return null }
 
     return (
-        <Link to={`/spec_ranking/${spec_slug}/${DEFAULT_BOSS}`} data-tooltip={spec.full_name}>
+        <Link to={`/spec_ranking/${spec_slug}/${DEFAULT_BOSS}/${DEFAULT_DIFF}`} data-tooltip={spec.full_name}>
             <Icon spec={spec} alt={spec.full_name} />
         </Link>
     )
