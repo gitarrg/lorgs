6c990c9d761bdbf3a887f1dbbfd0baf1cdcf6f20
gitarrg <not telling :)>
2022-02-06 22:57:29 +0000

    fix DH: Blur (havoc only!)

--- lorgs/data/classes/demonhunter.py
+++ lorgs/data/classes/demonhunter.py
@@ -28,8 +28,7 @@ DEMONHUNTER.add_spell(         spell_id=306830, cooldown=60,                colo
 DEMONHUNTER.add_spell(         spell_id=323639, cooldown=90,   duration=6,  color=COL_NF,    name="The Hunt",        icon="ability_ardenweald_demonhunter.jpg")
 DEMONHUNTER.add_debuff(        spell_id=317009,                             color=COL_VENTR, name="Sinful Brand",    icon="ability_revendreth_demonhunter.jpg")
 
-DEMONHUNTER.add_spell(        spell_id=198589,  cooldown=60,   duration=10,                  name="Blur",            icon="ability_demonhunter_blur.jpg", show=False)
-
+DEMONHUNTER_HAVOC.add_spell(   spell_id=198589, cooldown=60,   duration=10,                  name="Blur",            icon="ability_demonhunter_blur.jpg", show=False)
 DEMONHUNTER_HAVOC.add_spell(   spell_id=200166, cooldown=240,  duration=30, color="#348540", name="Metamorphosis",   icon="ability_demonhunter_metamorphasisdps.jpg")
 DEMONHUNTER_HAVOC.add_spell(   spell_id=196718, cooldown=180,  duration=8,                   name="Darkness",        icon="ability_demonhunter_darkness.jpg",          show=False, spell_type=SPELL_TYPE_RAID)
 DEMONHUNTER_HAVOC.add_spell(   spell_id=196555, cooldown=180,  duration=5,                   name="Netherwalk",      icon="spell_warlock_demonsoul.jpg",               show=False)
