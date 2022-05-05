ae51cbc4c1d4daddb94169549e70c6b04431c8a9
gitarrg <not telling :)>
2022-05-05 13:22:47 +0100

    Paladin: Fix Ardent Defender Buff Uptime + Wings CD

--- lorgs/data/classes/paladin.py
+++ lorgs/data/classes/paladin.py
@@ -28,7 +28,7 @@ PALADIN_RETRIBUTION = WowSpec(role=MDPS, wow_class=PALADIN, name="Retribution", 
 #
 PALADIN.add_spell(             spell_id=304971, cooldown=60,               color=COL_KYR,   name="Divine Toll",                     icon="ability_bastion_paladin.jpg",               show=False)
 PALADIN.add_spell(             spell_id=316958, cooldown=240, duration=30, color=COL_VENTR, name="Ashen Hallow",                    icon="ability_revendreth_paladin.jpg")
-PALADIN.add_spell(             spell_id=31884,  cooldown=120, duration=20, color="#ffc107", name="Avenging Wrath",                  icon="spell_holy_avenginewrath.jpg")
+PALADIN.add_spell(             spell_id=31884,  cooldown=60,  duration=20, color="#ffc107", name="Avenging Wrath",                  icon="spell_holy_avenginewrath.jpg", tags=[WowSpell.TAG_DYNAMIC_CD])
 PALADIN.add_spell(             spell_id=6940,   cooldown=120, duration=12,                  name="Blessing of Sacrifice",           icon="spell_holy_sealofsacrifice.jpg", show=False)
 
 # Defensive
@@ -39,7 +39,7 @@ PALADIN_HOLY.add_spell(        spell_id=498,    cooldown=60,  duration=8,       
 # Offensive
 PALADIN_HOLY.add_spell(        spell_id=31821,  cooldown=180, duration=8,  color="#dc65f5", name="Aura Mastery",                    icon="spell_holy_auramastery.jpg")
 
-PALADIN_PROTECTION.add_spell(  spell_id=31850,  cooldown=120, duration=8,  color="#fcea74", name="Ardent Defender",                 icon="spell_holy_ardentdefender.jpg")
+PALADIN_PROTECTION.add_buff(   spell_id=31850,  cooldown=60,  duration=8,  color="#fcea74", name="Ardent Defender",                 icon="spell_holy_ardentdefender.jpg", tags=[WowSpell.TAG_DYNAMIC_CD])
 PALADIN_PROTECTION.add_spell(  spell_id=212641, cooldown=300, duration=8,                   name="Guardian of Ancient Kings",       icon="spell_holy_heroism.jpg")
 PALADIN_PROTECTION.add_spell(  spell_id=152262, cooldown=60,  duration=15, color="#0a60ff", name="Seraphim",                        icon="ability_paladin_seraphim.jpg")
 
