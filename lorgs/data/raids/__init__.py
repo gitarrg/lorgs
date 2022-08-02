b713fce003b6342597c4e9dfb67883dbe7c48817
gitarrg <not telling :)>
2022-08-03 00:49:55 +0100

    change default raid

--- lorgs/data/raids/__init__.py
+++ lorgs/data/raids/__init__.py
@@ -4,4 +4,4 @@ from lorgs.data.raids.t26_castle_nathria import *
 from lorgs.data.raids.t28_sanctum_of_domination import *
 from lorgs.data.raids.t29_sepulcher_of_the_first_ones import *
 
-CURRENT_ZONE = SEPULCHER_OF_THE_FIRST_ONES
+CURRENT_ZONE = CASTLE_NATHRIA
