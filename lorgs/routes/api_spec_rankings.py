05d02049a2aad2dc654d739709d39f926f74a54f
gitarrg <not telling :)>
2022-08-13 01:52:58 +0100

    fix missing import

--- lorgs/routes/api_spec_rankings.py
+++ lorgs/routes/api_spec_rankings.py
@@ -7,8 +7,8 @@ from lorgs import data
 from lorgs.logger import logger
 from lorgs.models import warcraftlogs_ranking
 from lorgs.models.wow_spec import WowSpec
+from lorgs.models.raid_boss import RaidBoss
 from lorgs.routes import api_tasks
-
 
 
 router = fastapi.APIRouter(tags=["spec_rankings"])
