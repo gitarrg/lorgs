454b54d53b4502ef71b395b7866e7734e0e5ac59
gitarrg <not telling :)>
2022-10-05 13:26:49 +0100

    add Dathea Spells

--- lorgs/data/raids/t31_vault_of_the_incarnates.py
+++ lorgs/data/raids/t31_vault_of_the_incarnates.py
@@ -80,6 +80,29 @@ SENNARTH = VAULT_OF_THE_INCARNATES.add_boss(id=2592, name="Sennarth, The Cold Br
 # 05: Dathea, Ascended
 DATHEA = VAULT_OF_THE_INCARNATES.add_boss(id=2635, name="Dathea, Ascended", nick="Dathea")
 
+# Tornados
+DATHEA.add_cast(
+    spell_id=388410, name="Crosswinds", duration=4, show=False,
+    color="#6fd1c4", icon="inv_10_jewelcrafting_bg_air.jpg",
+)
+
+# Suck in
+DATHEA.add_cast(
+    spell_id=376943, name="Cyclone", duration=4+10,
+    color="#d18726", icon="creatureportrait_cyclone_nodebris.jpg",
+)
+
+# Summon Adds
+DATHEA.add_cast(
+    spell_id=387849, name="Coalescing Storm", duration=5,
+    color="#cfcf23", icon="inv_10_elementalspiritfoozles_air.jpg",
+)
+
+# Knockback
+DATHEA.add_cast(
+    spell_id=391382, name="Blowback", duration=4,
+    color="#50cc2b", icon="inv_misc_volatileair.jpg",
+)
 
 ################################################################################
 # 06: Kurog Grimtotem
