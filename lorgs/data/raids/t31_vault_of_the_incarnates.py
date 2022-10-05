a496bec53fe73e6f19bb217cb52a585dbd00343b
gitarrg <not telling :)>
2022-10-05 15:40:35 +0100

    add Kurog Spells

--- lorgs/data/raids/t31_vault_of_the_incarnates.py
+++ lorgs/data/raids/t31_vault_of_the_incarnates.py
@@ -104,9 +104,34 @@ DATHEA.add_cast(
     color="#50cc2b", icon="inv_misc_volatileair.jpg",
 )
 
+
 ################################################################################
 # 06: Kurog Grimtotem
+RED_1 = "#bf3030" # hsv(0, 0.75, 0.75)
+
 KUROG = VAULT_OF_THE_INCARNATES.add_boss(id=2605, name="Kurog Grimtotem", nick="Kurog")
+
+# Intermission
+KUROG.add_buff(spell_id=374779, name="Primal Barrier", color="#30bf30", icon="inv_10_elementalcombinedfoozles_primordial.jpg")
+
+# Fire
+KUROG.add_cast(spell_id=382563, name="Magma Burst", duration=1.5 + 10, color="#FFF", icon="ability_rhyolith_magmaflow_wave.jpg")
+KUROG.add_cast(spell_id=374022, name="Searing Carnage", duration=3 + 5, color=RED_1, icon="spell_fire_moltenblood.jpg")
+
+# Frost
+KUROG.add_cast(spell_id=373678, name="Biting Chill", duration=10, color="#63c4c9", icon="spell_frost_arcticwinds.jpg")
+KUROG.add_cast(spell_id=391019, name="Frigid Torrent", duration=2+4, color="#63c4c9", icon="spell_frost_ring-of-frost.jpg", show=False)
+KUROG.add_cast(spell_id=372456, name="Absolute Zero", duration=2+6, color=RED_1, icon="spell_frost_glacier.jpg")
+
+# Earth
+KUROG.add_cast(spell_id=390796, name="Erupting Bedrock", duration=5, color="#bf8330", icon="spell_shaman_earthquake.jpg", show=False)
+KUROG.add_cast(spell_id=391055, name="Enveloping Earth", duration=1.5, color="#bf8330", icon="inv_elementalearth2.jpg")
+KUROG.add_cast(spell_id=374691, name="Seismic Rupture", duration=5, color=RED_1, icon="spell_nature_earthquake.jpg")
+
+# Storm
+KUROG.add_cast(spell_id=390920, name="Shocking Burst", duration=5, color="#30bfa0", icon="spell_nature_unrelentingstorm.jpg")
+KUROG.add_cast(spell_id=373487, name="Lightning Crash", duration=5, color="#30bfa0", icon="spell_shaman_crashlightning.jpg")
+KUROG.add_cast(spell_id=374215, name="Thunder Strike", duration=7, color=RED_1, icon="ability_vehicle_electrocharge.jpg")
 
 
 ################################################################################
