3416bb5ed87a1fe4e1cd79fa1e883a30356b3bd6
gitarrg <not telling :)>
2022-10-30 19:10:51 +0000

    cleanup

--- test/helpers.py
+++ test/helpers.py
@@ -4,9 +4,22 @@ import os
 import json
 
 
-def load_fixture(file_name):
+ROOT = "./test/fixtures/"
 
-    file_path = os.path.join("./test/fixtures/", file_name)
+
+def save_fixture(filename: str, content):
+
+    file_path = os.path.join(ROOT, filename)
+    with open(file_path, "w", encoding="utf-8") as json_file:
+        return json.dump(
+            content, json_file,
+            indent=4,
+        )
+
+
+def load_fixture(filename: str):
+
+    file_path = os.path.join(ROOT, filename)
     with open(file_path, "rb") as json_file:
         return json.load(json_file)
 
