53bcc9c8f0a0366c40bddb762dbf8b6e6b96d89a
gitarrg <not telling :)>
2022-11-08 04:56:28 +0000

    SQS: update spec ranking handler

--- lorrgs_sqs/task_handlers/load_spec_rankings.py
+++ lorrgs_sqs/task_handlers/load_spec_rankings.py
@@ -10,39 +10,58 @@ from lorgs.models import warcraftlogs_ranking
 
 
 async def load_spec_rankings(
-    boss_slug: str, spec_slug: str,
-    difficulty = "mythic", metric = "dps",
-    limit = 50, clear = False,
-    **kwargs, # pylint: disable=unused-argument
-):
+    boss_slug: str,
+    spec_slug: str,
+    difficulty: str = "mythic",
+    metric: str = "dps",
+    limit=50,
+    clear=False,
+) -> tuple[bool, str]:
     """Load the Spec Ranking Data from Warcraftlogs and save it to the Database."""
     ################################
     # Get inputs
+
+    # fmt: off
     print(f"loading: {boss_slug} vs {spec_slug} | (difficulty={difficulty} / metric={metric} / limit={limit} / clear={clear})")
+    # fmt: on
     if boss_slug is None or spec_slug is None:
-        return f"missing boss or spec ({boss_slug} / {spec_slug})"
+        return False, f"missing boss or spec ({boss_slug} / {spec_slug})"
 
     ################################
     # get spec ranking object
-    ranking = warcraftlogs_ranking.SpecRanking.get_or_create(
+    ranking = warcraftlogs_ranking.SpecRanking.get(
         boss_slug=boss_slug,
         spec_slug=spec_slug,
         difficulty=difficulty,
         metric=metric,
+        create=True,
     )
     if not ranking.boss:
-        return "invalid boss"
+        return False, "invalid boss"
     if not ranking.spec:
-        return "invalid spec"
+        return False, "invalid spec"
 
     ################################
     # load and save
     await ranking.load(limit=limit, clear_old=clear)
     ranking.save()
-    return "done"
+    return True, "done"
 
 
-async def main(message):
+async def main(message: dict[str, str]):
+    """Load the Spec Ranking Data from Warcraftlogs and save it to the Database."""
+
+    # parse the message
     message_body = message.get("body")
-    message_payload = json.loads(message_body)
-    await load_spec_rankings(**message_payload)
+    if not message_body:
+        return False, "No message body."
+    payload = json.loads(message_body)
+
+    return await load_spec_rankings(
+        boss_slug=payload.get("boss_slug"),
+        spec_slug=payload.get("spec_slug"),
+        difficulty=payload.get("difficulty", "mythic"),
+        metric=payload.get("metric", "dps"),
+        limit=payload.get("limit", 50),
+        clear=payload.get("clear", False),
+    )
