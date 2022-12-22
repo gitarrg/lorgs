593a1daf001479bc81ac8e4ef5febcea79f7df3a
gitarrg <not telling :)>
2022-12-22 15:54:12 +0000

    Druid: fix ToL Spell ID

--- lorgs/data/classes/druid.py
+++ lorgs/data/classes/druid.py
@@ -66,7 +66,7 @@ DRUID_GUARDIAN.add_spell(    spell_id=22812,  cooldown=60,  duration=8,         
 
 DRUID_RESTORATION.add_spell( spell_id=197721, cooldown=90,  duration=8,  color="#7ec44d", name="Flourish",                       icon="spell_druid_wildburst.jpg",                 show=False)
 DRUID_RESTORATION.add_spell( spell_id=740,    cooldown=120, duration=6,  color="#6cbfd9", name="Tranquility",                    icon="spell_nature_tranquility.jpg")
-DRUID_RESTORATION.add_buff(  spell_id=33891,  cooldown=0,   duration=9,                   name="Incarnation: Tree of Life",      icon="ability_druid_improvedtreeform.jpg", wowhead_data="spell=33891")
+DRUID_RESTORATION.add_buff(  spell_id=117679, cooldown=0,   duration=9,                   name="Incarnation: Tree of Life",      icon="ability_druid_improvedtreeform.jpg", wowhead_data="spell=33891")
 
 
 # Additional Spells (not tracked)
