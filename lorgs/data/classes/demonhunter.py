26bbef223ef658e3b3b78b948e6bdb6822b8c304
gitarrg <not telling :)>
2022-12-23 15:00:23 +0000

    DH: add Vengeful Retreat

--- lorgs/data/classes/demonhunter.py
+++ lorgs/data/classes/demonhunter.py
@@ -28,6 +28,7 @@ DEMONHUNTER_VENGEANCE = WowSpec(role=TANK, wow_class=DEMONHUNTER, name="Vengeanc
 DEMONHUNTER.add_spell(         spell_id=306830, cooldown=60,                color=COL_KYR,   name="Elysian Decree",  icon="ability_bastion_demonhunter.jpg",           show=False)
 DEMONHUNTER.add_spell(         spell_id=370966, cooldown=90,   duration=6,  color="#2075d6", name="The Hunt",        icon="ability_ardenweald_demonhunter.jpg")
 DEMONHUNTER.add_spell(         spell_id=196718, cooldown=180,  duration=8,                   name="Darkness",        icon="ability_demonhunter_darkness.jpg",          show=False, spell_type=SPELL_TYPE_RAID)
+DEMONHUNTER.add_spell(         spell_id=198793, cooldown=25,                color="#c95bcf", name="Vengeful Retreat", icon="ability_demonhunter_vengefulretreat2.jpg", show=False)
 
 DEMONHUNTER_HAVOC.add_spell(   spell_id=198589, cooldown=60,   duration=10,                  name="Blur",            icon="ability_demonhunter_blur.jpg", show=False)
 DEMONHUNTER_HAVOC.add_spell(   spell_id=258860, cooldown=40,   duration=4,  color="#9177fc", name="Essence Break",   icon="spell_shadow_ritualofsacrifice.jpg",               show=False)
