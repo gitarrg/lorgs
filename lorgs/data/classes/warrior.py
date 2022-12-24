558bb7ef3b745bb5522af09caad4aa4e3df3c412
gitarrg <not telling :)>
2022-12-24 01:49:25 +0000

    Warrior: various fixes

--- lorgs/data/classes/warrior.py
+++ lorgs/data/classes/warrior.py
@@ -26,13 +26,12 @@ WARRIOR_PROTECTION    = WowSpec(role=TANK, wow_class=WARRIOR,      name="Protect
 ################################################################################
 # Spells
 #
-WARRIOR.add_spell(             spell_id=324143, cooldown=120, duration=15, color=COL_NECRO, name="Conqueror's Banner",  icon="ability_maldraxxus_warriorplantbanner.jpg")
-WARRIOR.add_spell(             spell_id=325886, cooldown=75,  duration=12, color=COL_NF,    name="Ancient Aftershock",  icon="ability_ardenweald_warrior.jpg")  # 15sec CD reduction with Conduit
-WARRIOR.add_spell(             spell_id=307865, cooldown=60,               color=COL_KYR,   name="Spear of Bastion",    icon="ability_bastion_warrior.jpg")
+WARRIOR.add_spell(             spell_id=376080, cooldown=90,               color=COL_KYR,   name="Spear of Bastion",    icon="ability_bastion_warrior.jpg")
+WARRIOR.add_spell(             spell_id=385954, cooldown=90,  duration=8,  color="#d13b59", name="Thunderous Roar",     icon="ability_warrior_dragonroar.jpg")
 WARRIOR.add_spell(             spell_id=97462,  cooldown=180, duration=10,                  name="Rallying Cry",        icon="ability_warrior_rallyingcry.jpg",           show=False, spell_type=SPELL_TYPE_RAID)
 WARRIOR.add_spell(             spell_id=23920,  cooldown=10,  duration=5,                   name="Spell Reflection",    icon="ability_warrior_shieldreflection.jpg",      show=False)
+WARRIOR.add_spell(             spell_id=107574, cooldown=90,  duration=20,                  name="Avatar",              icon="warrior_talent_icon_avatar.jpg")
 
-WARRIOR_ARMS.add_spell(        spell_id=107574, cooldown=90,  duration=20,                  name="Avatar",              icon="warrior_talent_icon_avatar.jpg")
 WARRIOR_ARMS.add_spell(        spell_id=227847, cooldown=90,  duration=6,                   name="Bladestorm",          icon="ability_warrior_bladestorm.jpg")
 WARRIOR_ARMS.add_spell(        spell_id=262161, cooldown=45,  duration=10,                  name="Warbreaker",          icon="inv_warbreaker.jpg",                        show=False)
 WARRIOR_ARMS.add_spell(        spell_id=772,    cooldown=15,                                name="Rend",                icon="ability_gouge.jpg",                         show=False)
@@ -43,6 +42,8 @@ WARRIOR_FURY.add_spell(        spell_id=1719,   cooldown=60,  duration=10,      
 WARRIOR_FURY.add_spell(        spell_id=46924,  cooldown=60,  duration=4,                   name="Bladestorm",          icon="ability_warrior_bladestorm.jpg")
 WARRIOR_FURY.add_spell(        spell_id=184364, cooldown=180, duration=8,                   name="Enraged Regeneration",icon="ability_warrior_focusedrage.jpg",           show=False)
 
+WARRIOR_PROTECTION.add_spell(  spell_id=385954, cooldown=45,               color="#b34747", name="Shield Charge",       icon="ability_warrior_shieldcharge.jpg")
+WARRIOR_PROTECTION.add_spell(  spell_id=385954, cooldown=90,  duration=12, color="#d1793b", name="Ravager",             icon="warrior_talent_icon_ravager.jpg")
 WARRIOR_PROTECTION.add_spell(  spell_id=107574, cooldown=50,  duration=20,                  name="Avatar",              icon="warrior_talent_icon_avatar.jpg",            show=False)  # CD reduced by Talent per Rage spend
 WARRIOR_PROTECTION.add_spell(  spell_id=12975,  cooldown=180, duration=15, color="#ffbf29", name="Last Stand",          icon="spell_holy_ashestoashes.jpg")
 WARRIOR_PROTECTION.add_spell(  spell_id=871,    cooldown=120, duration=8,  color="#039dfc", name="Shield Wall",         icon="ability_warrior_shieldwall.jpg")  # CD reduced by Talent per Rage spend
