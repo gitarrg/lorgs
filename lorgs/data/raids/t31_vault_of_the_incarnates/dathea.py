1f6b90ef95cc389c155b53304165f1ad8554e18d
gitarrg <not telling :)>
2022-12-26 16:00:06 +0000

    Dathea: add Conductive Mark

--- lorgs/data/raids/t31_vault_of_the_incarnates/dathea.py
+++ lorgs/data/raids/t31_vault_of_the_incarnates/dathea.py
@@ -5,26 +5,50 @@ from lorgs.models.raid_boss import RaidBoss
 
 DATHEA = RaidBoss(id=2635, name="Dathea, Ascended", nick="Dathea")
 
+# Debuffs
+DATHEA.add_cast(
+    spell_id=391600,
+    name="Conductive Mark",
+    duration=15,
+    color="#297bcc",
+    icon="spell_shaman_staticshock.jpg",
+    show=False,
+)
+
+
 # Tornados
 DATHEA.add_cast(
-    spell_id=388410, name="Crosswinds", duration=4, show=False,
-    color="#6fd1c4", icon="inv_10_jewelcrafting_bg_air.jpg",
+    spell_id=388410,
+    name="Crosswinds",
+    duration=4,
+    show=False,
+    color="#6fd1c4",
+    icon="inv_10_jewelcrafting_bg_air.jpg",
 )
 
 # Suck in
 DATHEA.add_cast(
-    spell_id=376943, name="Cyclone", duration=4+10,
-    color="#d18726", icon="creatureportrait_cyclone_nodebris.jpg",
+    spell_id=376943,
+    name="Cyclone",
+    duration=4 + 10,
+    color="#cc3d3d",
+    icon="creatureportrait_cyclone_nodebris.jpg",
 )
 
 # Summon Adds
 DATHEA.add_cast(
-    spell_id=387849, name="Coalescing Storm", duration=5,
-    color="#cfcf23", icon="inv_10_elementalspiritfoozles_air.jpg",
+    spell_id=387849,
+    name="Coalescing Storm",
+    duration=5 + 45, # ~45sec in mythic
+    color="#47663d",
+    icon="inv_10_elementalspiritfoozles_air.jpg",
 )
 
 # Knockback
 DATHEA.add_cast(
-    spell_id=391382, name="Blowback", duration=4,
-    color="#50cc2b", icon="inv_misc_volatileair.jpg",
+    spell_id=391382,
+    name="Blowback",
+    duration=4,
+    color="#d18726",
+    icon="inv_misc_volatileair.jpg",
 )
