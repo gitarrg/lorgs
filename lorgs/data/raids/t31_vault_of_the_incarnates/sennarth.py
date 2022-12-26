a9c0435c25faff2ee1ea25559a58920967876563
gitarrg <not telling :)>
2022-12-26 15:40:48 +0000

    Sennarth: Add big Add AoE

--- lorgs/data/raids/t31_vault_of_the_incarnates/sennarth.py
+++ lorgs/data/raids/t31_vault_of_the_incarnates/sennarth.py
@@ -9,23 +9,53 @@ SENNARTH = RaidBoss(id=2592, name="Sennarth, The Cold Breath", nick="Sennarth")
 ##############
 # P1
 
+# Spread Debuffs
+SENNARTH.add_cast(
+    spell_id=371976,
+    name="Chilling Blast",
+    duration=2 + 5,
+    color="#2996cc",
+    icon="spell_fire_blueflamering.jpg",
+)
+
+
+# Big Add AoE
+SENNARTH.add_cast(
+    spell_id=373817,
+    name="Chilling Aura",
+    duration=2 + 5,
+    color="#45e6cb",
+    icon="spell_frost_coldhearted.jpg",
+)
+
+# Grip
+SENNARTH.add_cast(
+    spell_id=373405,
+    name="Gossamer Burst",
+    duration=3 + 6,
+    color="#ffd500",
+    icon="priest_icon_chakra.jpg",
+)
+
+
 # Spiders
 SENNARTH.add_cast(
-    spell_id=372238, name="Call Spiderlings", duration=9,
-    color="#60bf30", icon="inv_misc_monsterspidercarapace_01.jpg",
+    spell_id=372238,
+    name="Call Spiderlings",
+    duration=9,
+    color="#60bf30",
+    icon="inv_misc_monsterspidercarapace_01.jpg",
+    show=False,
 )
 
 # Web
 SENNARTH.add_cast(
-    spell_id=372082, name="Enveloping Webs", duration=1+6,
+    spell_id=372082,
+    name="Enveloping Webs",
+    duration=1 + 6,
     show=False,
-    color="#9060bf", icon="inv_misc_web_01.jpg",
-)
-
-
-SENNARTH.add_cast(
-    spell_id=373405, name="Gossamer Burst", duration=3+6,
-    color="#ffd500", icon="priest_icon_chakra.jpg",
+    color="#af60bf",
+    icon="inv_misc_web_01.jpg",
 )
 
 
@@ -34,12 +64,18 @@ SENNARTH.add_cast(
 
 # Enrage
 SENNARTH.add_cast(
-    spell_id=372648, name="Pervasive Cold", duration=6,
-    color="#bf3030", icon="ability_mage_chilledtothebone.jpg",
+    spell_id=372648,
+    name="Pervasive Cold",
+    duration=6,
+    color="#bf3030",
+    icon="ability_mage_chilledtothebone.jpg",
 )
 
 # Knockback
 SENNARTH.add_cast(
-    spell_id=373027, name="Suffocating Webs", duration=3+6,
-    color="#af60bf", icon="inv_misc_web_02.jpg",
+    spell_id=373027,
+    name="Suffocating Webs",
+    duration=3 + 6,
+    color="#af60bf",
+    icon="inv_misc_web_02.jpg",
 )
