fdbfd96a86c02e5d85e3fc824bc3aaf571f46bbe
gitarrg <not telling :)>
2023-01-23 14:14:32 +0000

    Externals: Remove Warrior's Safeguard

--- lorgs/data/externals.py
+++ lorgs/data/externals.py
@@ -25,7 +25,6 @@ OTHER_BUFFS.add_buff(color=PRIEST.color,  spell_id=47788,  cooldown=180,        
 OTHER_BUFFS.add_buff(color=PALADIN.color, spell_id=1022,   cooldown=300, duration=10, name="Blessing of Protection",  icon="spell_holy_sealofprotection.jpg")
 OTHER_BUFFS.add_buff(color=PALADIN.color, spell_id=6940,   cooldown=120,              name="Blessing of Sacrifice",   icon="spell_holy_sealofsacrifice.jpg")
 OTHER_BUFFS.add_buff(color=MONK.color,    spell_id=116849, cooldown=120,              name="Life Cocoon",             icon="ability_monk_chicocoon.jpg")
-OTHER_BUFFS.add_buff(color=WARRIOR.color, spell_id=335198, cooldown=30,               name="Safeguard",               icon="ability_warrior_safeguard.jpg")  # Warrior Intervene Conduit
 OTHER_BUFFS.add_buff(color=EVOKER.color,  spell_id=357170, cooldown=60,  duration=8,  name="Time Dilation",           icon="ability_evoker_timedilation.jpg")
 
 
