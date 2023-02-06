ac4adffed385bf152995ad6150672677ce818093
gitarrg <not telling :)>
2023-02-06 15:40:42 +0000

    Add Evoker Staff

--- lorgs/data/trinkets.py
+++ lorgs/data/trinkets.py
@@ -87,3 +87,16 @@ add_trinket(
     icon="shaman_talent_unleashedfury.jpg",
     wowhead_data=f"item=194308{mythic}&ilvl=424",
 )
+
+
+add_trinket(
+    EVOKER_DEVASTATION,
+    EVOKER_PRESERVATION,
+    spell_id=394927,
+    color="#af4dff",
+    cooldown=180,
+    duration=3,
+    name="Kharnalex, The First Light",
+    icon="inv_staff_2h_dragondungeon_c_02.jpg",
+    wowhead_data=f"item=195519{mythic}&ilvl=424",
+)
