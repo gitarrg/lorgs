030243d2e2b64c867137ed26b2f9524e6c0e4336
gitarrg <not telling :)>
2023-02-06 15:53:23 +0000

    Warrior: Add Demo Shout

--- lorgs/data/classes/warrior.py
+++ lorgs/data/classes/warrior.py
@@ -48,3 +48,4 @@ WARRIOR_PROTECTION.add_spell(  spell_id=385954, cooldown=45,               color
 WARRIOR_PROTECTION.add_spell(  spell_id=228920, cooldown=90,  duration=12, color="#d1793b", name="Ravager",             icon="warrior_talent_icon_ravager.jpg")
 WARRIOR_PROTECTION.add_spell(  spell_id=12975,  cooldown=180, duration=15, color="#ffbf29", name="Last Stand",          icon="spell_holy_ashestoashes.jpg")
 WARRIOR_PROTECTION.add_spell(  spell_id=871,    cooldown=120, duration=8,  color="#039dfc", name="Shield Wall",         icon="ability_warrior_shieldwall.jpg")  # CD reduced by Talent per Rage spend
+WARRIOR_PROTECTION.add_spell(  spell_id=1160,   cooldown=45,  duration=8,  color="#36b336", name="Demoralizing Shout",  icon="ability_warrior_warcry.jpg", show=False)
