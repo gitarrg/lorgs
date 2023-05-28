93e0880090117f4206d89b3b9ea210bd249ebf1d
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2023-05-28 15:00:09 +0100

    Paladin Prot: add Moment of Glory

--- lorgs/data/classes/paladin.py
+++ lorgs/data/classes/paladin.py
@@ -42,13 +42,14 @@ PALADIN_HOLY.add_spell(        spell_id=498,    cooldown=60,  duration=8,       
 PALADIN_HOLY.add_spell(        spell_id=31821,  cooldown=180, duration=8,  color="#dc65f5", name="Aura Mastery",                    icon="spell_holy_auramastery.jpg", tags=[SpellTag.RAID_CD])
 PALADIN_HOLY.add_spell(        spell_id=200025, cooldown=15,  duration=8,  color="#e3ca09", name="Beacon of Virtue",                icon="ability_paladin_beaconofinsight.jpg", show=False)
 PALADIN_HOLY.add_spell(        spell_id=216331, cooldown=45,  duration=12, color="#ffc107", name="Avenging Crusader",               icon="ability_paladin_veneration.jpg")
-PALADIN_HOLY.add_spell(        spell_id=205191, cooldown=60,  duration=14, color="#fcea74", name="Light's Hammer",                  icon="spell_paladin_lightshammer.jpg", show=False)
+PALADIN_HOLY.add_spell(        spell_id=114158, cooldown=60,  duration=14, color="#fcea74", name="Light's Hammer",                  icon="spell_paladin_lightshammer.jpg", show=False)
 
 
 PALADIN_PROTECTION.add_buff(   spell_id=31850,  cooldown=120, duration=8,  color="#fcea74", name="Ardent Defender",                 icon="spell_holy_ardentdefender.jpg")
 PALADIN_PROTECTION.add_spell(  spell_id=212641, cooldown=300, duration=8,                   name="Guardian of Ancient Kings",       icon="spell_holy_heroism.jpg", variations=[86659])
 PALADIN_PROTECTION.add_spell(  spell_id=152262, cooldown=60,  duration=15, color="#0a60ff", name="Seraphim",                        icon="ability_paladin_seraphim.jpg")
-PALADIN_PROTECTION.add_spell(  spell_id=387174, cooldown=60,  duration=9,  color="#40bfff", name="Eye of Tyr",                        icon="inv_shield_1h_artifactnorgannon_d_01.jpg")
+PALADIN_PROTECTION.add_spell(  spell_id=387174, cooldown=60,  duration=9,  color="#40bfff", name="Eye of Tyr",                      icon="inv_shield_1h_artifactnorgannon_d_01.jpg")
+PALADIN_PROTECTION.add_spell(  spell_id=327193, cooldown=60,  duration=15, color="#ffc107", name="Moment of Glory",                 icon="spell_holy_aspiration.jpg", show=False)
 
 
 PALADIN_RETRIBUTION.add_spell( spell_id=255937, cooldown=45,  duration=15, color="#ff6e07", name="Wake of Ashes",      icon="inv_sword_2h_artifactashbringerfire_d_03.jpg")
