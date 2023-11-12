78dcffbb5293938bec17e7116606f46259d6df66
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2023-11-12 14:39:36 +0000

    add Volcoross

--- lorgs/data/raids/t35_amdrassil/volcoross.py
+++ lorgs/data/raids/t35_amdrassil/volcoross.py
@@ -5,3 +5,22 @@ from lorgs.models.raid_boss import RaidBoss
 
 VOLCOROSS = RaidBoss(id=2737, name="Volcoross")
 boss = VOLCOROSS
+
+
+# debuffs
+boss.add_cast(
+    spell_id=421284,
+    name="Coiling Flames",
+    duration=1.6,
+    color="hsl(270, 60%, 70%)",
+    icon="ability_evoker_firestorm.jpg",
+)
+
+# group soaks
+boss.add_cast(
+    spell_id=420933,
+    name="Flood of the Firelands",
+    duration=5,
+    color="#d1c21f",
+    icon="spell_shaman_lavasurge.jpg",
+)
