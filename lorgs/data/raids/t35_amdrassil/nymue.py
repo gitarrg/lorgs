208a4f8625aaab933f421eaa3528aab0cf49a8c5
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2023-11-12 15:51:32 +0000

    add Nymue

--- lorgs/data/raids/t35_amdrassil/nymue.py
+++ lorgs/data/raids/t35_amdrassil/nymue.py
@@ -5,3 +5,47 @@ from lorgs.models.raid_boss import RaidBoss
 
 NYMUE = RaidBoss(id=2708, name="Nymue, Weaver of the Cycle", nick="Nymue")
 boss = NYMUE
+
+
+################################################################################
+# Main Phase
+#
+
+# Continuum = AOE ????
+# boss.add_cast(
+#     spell_id=420846,
+#     name="Continuum",
+#     duration=6.5 + 10,
+#     color="hsl(0, 50%, 50%)",
+#     icon="inv_legion_faction_dreamweavers.jpg",
+# )
+
+# Impeding Loom = Dodge Lines from Boss
+
+
+# Surging Growths = Soaks
+
+
+# Viridian Rain = spread cicles on some players
+boss.add_cast(
+    spell_id=420907,
+    name="Viridian Rain",
+    duration=1.5 + 6,
+    color="hsl(175, 50%, 50%)",
+    icon="ability_evoker_dreamflight.jpg",
+)
+
+
+# Barrier Blossoms = Circle -> Area Denial == Drop Far
+
+
+################################################################################
+# Intermission
+#
+
+boss.add_buff(
+    spell_id=413443,
+    name="Life Ward",
+    color="hsl(120, 50%, 50%)",
+    icon="spell_nature_skinofearth.jpg",
+)
