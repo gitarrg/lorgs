a8f4302e509475c37d072b06d3c0ba0396806bc6
arrg <arrg@lorrgs.io>
2023-12-12 14:00:31 +0000

    Druid: add Survival Instincts for Feral

--- lorgs/data/classes/druid.py
+++ lorgs/data/classes/druid.py
@@ -48,6 +48,10 @@ DRUID_BALANCE.add_buff(BEAR_FORM)
 DRUID_FERAL.add_buff(BEAR_FORM)
 DRUID_RESTORATION.add_buff(BEAR_FORM)
 
+SURVIVAL_INSTINCTS = WowSpell(spell_id=61336,  cooldown=180, duration=6,                   name="Survival Instincts",             icon="ability_druid_tigersroar.jpg")
+DRUID_FERAL.add_spell(SURVIVAL_INSTINCTS)
+DRUID_GUARDIAN.add_spell(SURVIVAL_INSTINCTS)
+
 
 # Offensive
 DRUID_BALANCE.add_spell(     spell_id=194223, cooldown=180, duration=20,                  name="Celestial Alignment",            icon="spell_nature_natureguardian.jpg")
@@ -61,6 +65,7 @@ DRUID_FERAL.add_spell(       spell_id=106951, cooldown=120, duration=15,        
 DRUID_FERAL.add_spell(       spell_id=58984,  cooldown=120,              color="#999999", name="Shadowmeld ",                    icon="ability_ambush.jpg",                        show=False)
 DRUID_FERAL.add_spell(       spell_id=108291, cooldown=300, duration=45, color="#fcdf03", name="Hearth of the Wild ",            icon="spell_holy_blessingofagility.jpg")
 DRUID_FERAL.add_buff(        spell_id=197625,                            color="#11cff5", name="Moonkin Form ",                  icon="spell_nature_forceofnature.jpg")
+
 
 DRUID_GUARDIAN.add_spell(    spell_id=108292, cooldown=300, duration=45, color="#fcdf03", name="Heart of the Wild",              icon="spell_holy_blessingofagility.jpg",          show=False)
 DRUID_GUARDIAN.add_spell(    spell_id=61336,  cooldown=180, duration=6,                   name="Survival Instincts",             icon="ability_druid_tigersroar.jpg")
