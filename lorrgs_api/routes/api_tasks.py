dfb4d5c64b6be00877d1429e4f5cee530cdd7626
arrg <arrg@lorrgs.io>
2024-01-04 22:52:27 +0000

    Add task-status Websocket Endpoint

--- lorrgs_api/routes/api_tasks.py
+++ lorrgs_api/routes/api_tasks.py
@@ -1,5 +1,6 @@
 """Get Information about Tasks."""
 # IMPORT THIRD PARTY LIBRARIES
+import asyncio
 import fastapi
 
 # IMPORT LOCAL LIBRARIES
@@ -9,16 +10,10 @@ from lorgs.models.task import Task
 router = fastapi.APIRouter(tags=["tasks"], prefix="/tasks")
 
 
-################################################################################
-# Task Status
-#
-@router.get("/{task_id}")
-async def get_task(response: fastapi.Response, task_id):
-    response.headers["Cache-Control"] = "no-cache"
-
+def _get_task_info(task_id: str) -> None | dict:
     task = Task.get(task_id=task_id)
     if not task:
-        return "Task not found.", 404
+        raise KeyError("Task not found")
 
     return {
         "task_id": task.key,
@@ -27,3 +22,44 @@ async def get_task(response: fastapi.Response, task_id):
         "updated": task.updated.isoformat(),
         "items": task.items,
     }
+
+
+################################################################################
+# Task Status
+#
+@router.get("/{task_id}")
+async def get_task(response: fastapi.Response, task_id: str):
+    response.headers["Cache-Control"] = "no-cache"
+    try:
+        return _get_task_info(task_id)
+    except KeyError:
+        return "Task not found.", 404
+
+
+@router.websocket("/{task_id}")
+async def get_task(websocket: fastapi.WebSocket, task_id: str):
+    """Websocket Connection to request a task's status.
+
+    Sends the updated Status every 1 second.
+    Stops after 100 iterations
+
+    """
+    await websocket.accept()
+
+    # max number of iterations before giving up
+    max_iterations = 100
+    n_err = 0
+
+    for _ in range(max_iterations):
+        try:
+            info = _get_task_info(task_id)
+        except KeyError:
+            await websocket.send_json({"status": "not found"})
+            n_err += 1
+            await asyncio.sleep(1 + n_err)  # auto-throttle based on number of errros to
+        else:
+            await websocket.send_json(info)
+            await asyncio.sleep(1)
+
+    else:
+        await websocket.send_json({"status": "timeout"})
