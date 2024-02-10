dec928510a74aff4105cc2dc570aeb4528ace6af
arrg <arrg@lorrgs.io>
2024-02-10 14:48:17 +0000

    Druid: add Renewal

--- lorgs/data/classes/druid.py
+++ lorgs/data/classes/druid.py
@@ -40,6 +40,7 @@ DRUID.add_spell(             spell_id=124974, cooldown=90,  duration=30,        
 
 # Defensives
 DRUID.add_spell(             spell_id=22812, cooldown=60, duration=12, name="Barkskin",                icon="spell_nature_stoneclawtotem.jpg",              show=False)
+DRUID.add_spell(             spell_id=108238, cooldown=90,             name="Renewal",                 icon="spell_nature_natureblessing.jpg",              show=False)
 
 BEAR_FORM = WowSpell(spell_id=5487, name="Bear Form", icon="ability_racial_bearform.jpg", show=False, event_type="applybuff")
 BEAR_FORM.spell_type = DRUID.name_slug
