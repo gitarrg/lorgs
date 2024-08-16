3b82db068a8b14395c1bd791761ef2ec95939847
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2024-08-16 11:46:44 +0100

    add Spells for Ovi'nax

--- lorgs/data/raids/tww1_nerubar_palace/ovinax.py
+++ lorgs/data/raids/tww1_nerubar_palace/ovinax.py
@@ -5,3 +5,40 @@ from lorgs.models.raid_boss import RaidBoss
 
 OVINAX = RaidBoss(id=2919, name="Broodtwister Ovi'nax", nick="Ovi'nax")
 boss = OVINAX
+
+
+# boss opens container + aoe
+boss.add_cast(
+    spell_id=442432,
+    name="Ingest Black Blood",
+    duration=1 + 5,
+    color="#bf4040",
+    icon="inv_achievement_raidnerubian_swarmmother.jpg",
+)
+
+
+# Purple Circle = Break Eggs
+boss.add_cast(
+    spell_id=442526,
+    name="Experimental Dosage",
+    duration=1.5 + 6,
+    color="#8917ca",
+    icon="spell_deathknight_bloodplague.jpg",
+)
+
+
+# Grey Circle = Dispel
+boss.add_cast(
+    spell_id=446344,
+    name="Sticky Web",
+    duration=5,  # fake duration
+    color="#ca17a3",
+    icon="inv_ability_web_debuff.jpg",
+    show=False,
+)
+# 446349 = Debuff
+# 446351 = too close to dispel target = get rooted
+
+
+# 441368 = Tank Dot
+# 441362 = Tank Dot #2
