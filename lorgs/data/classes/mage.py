50714834f15d25888c6b9b17608607412115ed83
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2024-10-22 20:10:57 +0100

    Mage: fix Mass Barrier CD

--- lorgs/data/classes/mage.py
+++ lorgs/data/classes/mage.py
@@ -36,7 +36,7 @@ MAGE.add_buff(        spell_id=382440, cooldown=60,  duration=4,  color=COL_NF, 
 MAGE.add_buff(         spell_id=45438,  cooldown=240,                               name="Ice Block",          icon="spell_frost_frost.jpg",                     show=False, tags=[SpellTag.DEFENSIVE])
 MAGE.add_buff(         spell_id=55342,  cooldown=120,                               name="Mirror Image",       icon="spell_magic_lesserinvisibilty.jpg",         show=False, tags=[SpellTag.DEFENSIVE])
 MAGE.add_buff(         spell_id=342246, cooldown=60,                                name="Alter Time",         icon="spell_mage_altertime.jpg",                  show=False, tags=[SpellTag.DEFENSIVE], variations=[342246])
-MAGE.add_spell(        spell_id=414660, cooldown=120,                               name="Mass Barrier",       icon="ability_racial_magicalresistance.jpg",      show=False, tags=[SpellTag.DEFENSIVE])
+MAGE.add_spell(        spell_id=414660, cooldown=180,                               name="Mass Barrier",       icon="ability_racial_magicalresistance.jpg",      show=False, tags=[SpellTag.DEFENSIVE])
 MAGE.add_spell(        spell_id=414658, cooldown=240, duration=6,                   name="Ice Cold",           icon="spell_fire_bluefire.jpg",                   show=False, tags=[SpellTag.DEFENSIVE])
 MAGE.add_buff(         spell_id=113862, cooldown=120,                               name="Greater Invisibility",icon="ability_mage_greaterinvisibility.jpg",     show=False, tags=[SpellTag.DEFENSIVE])
 
