ec452f65683c4f21acb0f9b652ce3c4c09b7476c
arrg <arrg@lorrgs.io>
2025-02-10 01:33:30 +0000

    Vexie + Cauldron Spells

--- lorgs/data/raids/tww2_undermine/cauldron.py
+++ lorgs/data/raids/tww2_undermine/cauldron.py
@@ -1,4 +1,9 @@
-"""02: Cauldron of Carnage"""
+"""02: Cauldron of Carnage
+
+
+>>> scripts/load_report.py "https://www.warcraftlogs.com/reports/DzLQG1Rkp4xtdJgh?fight=16"
+
+"""
 
 from lorgs.models.raid_boss import RaidBoss
 
@@ -10,3 +15,78 @@ CAULDRON = RaidBoss(
     icon="inv_11_arenaboss_colossalclash.jpg",
 )
 boss = CAULDRON
+
+
+################################################################################
+# Torq
+
+# small adds
+# Voltaic Image
+
+# Pools
+boss.add_cast(
+    spell_id=463900,
+    name="Thunderdrum Salvo",
+    duration=8,
+    color="hsl(170, 75%, 60%)",
+    icon="inv_11_arenaboss_thunderdrumsalvo.jpg",
+)
+
+# Tank Hit
+boss.add_cast(
+    spell_id=463798,
+    name="Lightning Bash",
+    duration=4,  # not sure how long the combo lasts
+    color="#478fb3",
+    icon="inv_gizmo_supersappercharge.jpg",
+    show=False,
+)
+
+
+################################################################################
+# Flarendo
+
+
+# Group Soak
+boss.add_cast(
+    spell_id=473650,
+    name="Scrapbomb",
+    duration=10,
+    color="hsl(20, 75%, 60%)",
+    icon="inv_10_engineering2_boxofbombs_friendly_color1.jpg",
+)
+
+# Beam
+boss.add_cast(
+    spell_id=472233,
+    name="Blastburn Roarcannon",
+    duration=3.5,
+    cooldown=3,
+    color="hsl(40, 75%, 60%)",
+    icon="spell_shaman_shockinglava.jpg",
+)
+
+# Tank hit
+boss.add_cast(
+    spell_id=1214190,
+    name="Eruption Stomp",
+    duration=4,
+    color="#478fb3",
+    icon="inv_gizmo_supersappercharge.jpg",
+    show=False,
+)
+
+# Spread
+# Molten Phlem
+
+
+################################################################################
+# 100% Energy -> (Intermission)
+
+boss.add_buff(
+    spell_id=465872,
+    name="Colossal Clash",
+    duration=20,
+    color="hsl(120, 70%, 50%)",
+    icon="inv_11_arenaboss_colossalclash.jpg",
+)
--- lorgs/data/raids/tww2_undermine/vexie.py
+++ lorgs/data/raids/tww2_undermine/vexie.py
@@ -1,4 +1,9 @@
-"""01: Vexie"""
+"""01: Vexie
+
+Example Fight:
+>>> scripts/load_report.py "https://www.warcraftlogs.com/reports/DzLQG1Rkp4xtdJgh?fight=10&type=casts&hostility=1&source=22"
+
+"""
 
 from lorgs.models.raid_boss import RaidBoss
 
@@ -10,3 +15,72 @@ VEXIE = RaidBoss(
     icon="inv_111_raid_achievement_vexieandthegeargrinders.jpg",
 )
 boss = VEXIE
+
+
+################################################################################
+# Main Phase
+
+
+# Spew Oil -> spread
+boss.add_cast(
+    spell_id=459671,
+    name="Spew Oil",
+    duration=12,
+    color="hsl(170, 60%, 75%)",
+    icon="spell_yorsahj_bloodboil_black.jpg",
+    show=False,
+)
+
+# drop fire puddles -> burn oil
+boss.add_cast(
+    spell_id=468207,
+    name="Incendiary Fire",
+    duration=6,
+    color="hsl(300, 50%, 50%)",
+    icon="spell_shaman_improvedfirenova.jpg",
+    show=False,
+)
+
+# Exhaust Fumes --> Raid Wide AOE
+boss.add_buff(
+    spell_id=468149,
+    name="Exhaust Fumes",
+    duration=6,  # the buff stays but the dmg is only 6sec
+    color="hsl(0, 50%, 50%)",
+    icon="inv_misc_dust.jpg",
+    event_type="applybuff, applybuffstack",
+)
+
+
+# Car Spawns
+boss.add_cast(
+    spell_id=459943,
+    name="Call Bikers",
+    duration=1,
+    color="hsl(50, 80%, 50%)",
+    icon="inv_viciousgoblintrike.jpg",
+)
+
+
+# Tank Buster
+boss.add_cast(
+    spell_id=459627,
+    name="Tank Buster",
+    duration=1.5,
+    cooldown=25,
+    color="#478fb3",
+    icon="ability_vehicle_siegeenginecharge.jpg",
+    show=False,
+)
+
+
+################################################################################
+# Burn Phase
+
+# Tune-Up / Breakdown
+boss.add_debuff(
+    spell_id=460603,
+    name="Mechanical Breakdown",
+    color="hsl(120, 70%, 50%)",
+    icon="ability_siege_engineer_detonate.jpg",
+)
