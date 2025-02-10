738d92947446c1a24150a17419a39e59c695ddd7
Vincent Ullmann <Vincent.Ullmann@googlemail.com>
2025-02-10 00:15:54 +0000

    allow multiple event types

--- test/test_utils.py
+++ test/test_utils.py
@@ -14,18 +14,5 @@ def test__str_int_list__empty_input():
     assert utils.str_int_list("") == []
 
 
-class TestGroupBy:
-    def test__group_by__empty_input(self):
-        items: list = []
-        assert utils.group_by(*items, keyfunc=lambda x: "") == {}
-
-    def test__group_by__1(self):
-
-        items = ["foo", "bar", "a", "b", "c"]
-
-        result = utils.group_by(*items, keyfunc=lambda x: len(x))
-        assert result == {3: ["foo", "bar"], 1: ["a", "b", "c"]}
-
-
 if __name__ == "__main__":
     pytest.main(sys.argv)
